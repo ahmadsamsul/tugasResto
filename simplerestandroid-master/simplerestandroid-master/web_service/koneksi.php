001144a327ec6ad9fa63cd6db8d9b93cf16fd192
ahmadsamsul <55912593+ahmadsamsul@users.noreply.github.com>
2020-04-25 01:00:20 -0700

    Add files via upload

---
+++ simplerestandroid-master/simplerestandroid-master/web_service/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id13006548_ayam');
+ define('PASS','12345');
+ define('DB','id13006548_ayam');
+
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
